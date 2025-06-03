e3bcada766d75bff0654e4f7dd3a59ba5204b73f
이준영 <ff1451@gmail.com>
2025-06-03 20:26:23 +0900

    refactor: trigger 로직 수정

--- src/components/UI/Modal/CenterModal/Trigger.tsx
+++ src/components/UI/Modal/CenterModal/Trigger.tsx
@@ -1,38 +1,37 @@
-import React, { ReactElement, MouseEvent, type HTMLAttributes } from 'react';
+import React, { ReactElement, MouseEvent, type ButtonHTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-interface TriggerProps extends HTMLAttributes<HTMLDivElement> {
-  onClick: () => void;
+interface TriggerChildProps {
+  onClick?: (event: MouseEvent<HTMLElement>) => void;
+}
+
+interface TriggerProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'> {
+  onClick: (event?: MouseEvent<HTMLElement>) => void;
   children: React.ReactNode;
   asChild?: boolean;
 }
 
-export default function Trigger({ onClick, children, className, asChild = false }: TriggerProps) {
+export default function Trigger({ onClick, children, className, asChild = false, ...rest }: TriggerProps) {
   if (asChild && React.isValidElement(children)) {
-    return React.cloneElement(
-      children as ReactElement<{
-        onClick?: (event: MouseEvent<HTMLElement>) => void;
-      }>,
-      {
-        onClick: (e: MouseEvent<HTMLElement>) => {
-          onClick();
+    const childElement = children as ReactElement<TriggerChildProps>;
 
-          const childOnClick = (
-            children as ReactElement<{
-              onClick?: (event: MouseEvent<HTMLElement>) => void;
-            }>
-          ).props.onClick;
-
-          if (childOnClick) {
-            childOnClick(e);
-          }
-        },
+    return React.cloneElement(childElement, {
+      onClick: (e: MouseEvent<HTMLElement>) => {
+        onClick(e);
+        if (childElement.props.onClick) {
+          childElement.props.onClick(e);
+        }
       },
-    );
+    });
   }
 
   return (
-    <button type="button" onClick={onClick} className={twMerge('inline-flex items-center justify-center', className)}>
+    <button
+      type="button"
+      onClick={onClick}
+      className={twMerge('inline-flex items-center justify-center', className)}
+      {...rest}
+    >
       {children}
     </button>
   );
