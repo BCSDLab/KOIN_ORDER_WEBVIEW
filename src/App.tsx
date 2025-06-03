02afe04b93f3e511a16ad5a4f2d214c4e0d07b09
정해성 <gotjd0230@naver.com>
2025-06-03 15:00:54 +0900

    remove: 사용하지 않는 컴포넌트 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DetailAddress from './pages/Delivery/Outside/DetailAddress';
 import OrderFinish from './pages/OrderFinish/OrderFinish';
 import PaymentLayout from '@/components/Layout/PaymentLayout';
-import CartPage from '@/pages/Cart/CartPage';
-import MainPage from '@/pages/Main/MainPage';
-import MenuPage from '@/pages/Menu/MenuPage';
 import Payment from '@/pages/Payment/index';
-import StoreDetailPage from '@/pages/StoreDetail/StoreDetailPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/store/:storeId" element={<StoreDetailPage />} />
-        <Route path="/store/:storeId/menu/:menuId" element={<MenuPage />} />
-        <Route path="/cart" element={<CartPage />} />
         <Route path="/payment" element={<PaymentLayout />}>
           <Route index element={<Payment />} />
         </Route>
