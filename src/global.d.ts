018ec8b88647083f24fe54356c7ed93396989f97
이준영 <ff1451@gmail.com>
2025-06-24 09:14:03 +0900

    feat: 브릿지 구현

---
+++ src/global.d.ts
@@ -0,0 +1,21 @@
+declare global {
+  interface Window {
+    webkit?: {
+      messageHandlers: {
+        [name: string]: { postMessage: (body: unknown) => void };
+      };
+    };
+    Android?: {
+      [method: string]: (...args: unknown[]) => unknown;
+    };
+    onNativeCallback?: (callbackId: string, result: unknown) => void;
+    onAndroidCallback?: (callbackId: string, result: unknown) => void;
+
+    NativeBridge?: {
+      call: (methodName: string, ...args: unknown[]) => Promise<unknown>;
+      handleCallback: (callbackId: string, result: unknown) => void;
+    };
+  }
+}
+
+export {};
