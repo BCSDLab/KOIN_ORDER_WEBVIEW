aa6d1e9531e96bc1c559e027e04c78895059663e
강성훈 <kshevl123@gmail.com>
2025-07-06 22:23:53 +0900

    feat: 외부 배달 주소 저장 API 연동

--- src/api/delivery/index.ts
+++ src/api/delivery/index.ts
@@ -1,5 +1,8 @@
 import { apiClient } from '..';
 import { AddressSearchRequest, AddressSearchResponse } from '@/api/delivery/entity';
+import { PostAddressObjType } from '@/stores/useOrderStore';
+
+const getToken = () => localStorage.getItem('token');
 
 export const getRoadNameAddress = async ({ keyword, currentPage, countPerPage }: AddressSearchRequest) => {
   const response = await apiClient.get<AddressSearchResponse>('address/search', {
@@ -9,5 +12,17 @@ export const getRoadNameAddress = async ({ keyword, currentPage, countPerPage }:
       countPerPage,
     },
   });
+  return response;
+};
+
+export const postUserDeliveryAddress = async (addressData: PostAddressObjType) => {
+  const token = getToken();
+  const response = await apiClient.post('delivery/address/off-campus', {
+    body: addressData,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
   return response;
 };
