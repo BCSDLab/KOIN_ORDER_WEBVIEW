352c562a274632883bad14b67fd7840dd85c50f3
정해성 <gotjd0230@naver.com>
2025-07-08 10:47:47 +0900

    feat: 뱃지 추가

--- src/pages/Shop/components/MenuOptions.tsx
+++ src/pages/Shop/components/MenuOptions.tsx
@@ -1,5 +1,6 @@
 import OptionSelects from './OptionSelects';
 import { OptionGroup } from '@/api/shop/entity';
+import Badge from '@/components/UI/Badge';
 
 interface MenuOptionsProps {
   optionGroups: OptionGroup[];
@@ -11,14 +12,26 @@ export default function MenuOptions({ optionGroups }: MenuOptionsProps) {
       {optionGroups.map((group) => (
         <div
           key={group.id}
-          className="flex w-full scroll-mt-[124px] flex-col rounded-xl border border-neutral-300 bg-white px-4 py-6"
+          className="flex w-full scroll-mt-[124px] flex-col rounded-xl border border-neutral-300 bg-white px-6 py-4"
         >
-          <span className="flex h-[1.8125rem] text-lg leading-[1.6] font-semibold">{group.name}</span>
-          {group.description && (
-            <span className="mb-3 flex h-[1.1875rem] text-[12px] leading-[1.6] font-normal text-neutral-500">
-              {group.description}
-            </span>
-          )}
+          <div className="flex items-center justify-between">
+            <div>
+              <span className="flex h-[1.8125rem] text-lg leading-[1.6] font-semibold">{group.name}</span>
+              {group.description && (
+                <span className="mb-3 flex h-[1.1875rem] text-[12px] leading-[1.6] font-normal text-neutral-500">
+                  {group.description}
+                </span>
+              )}
+            </div>
+            {group.isRequired && (
+              <Badge
+                label="필수"
+                color="primaryLight"
+                variant="outlined"
+                className="h-[1.4375rem] w-[2.3125rem] justify-center p-0 text-xs"
+              />
+            )}
+          </div>
           <OptionSelects options={group.options} />
         </div>
       ))}
