59a366663eacf016098b9e88c6b4becbf1f97aa6
이준영 <ff1451@gmail.com>
2025-07-09 11:24:45 +0900

    fix: shop 파일 제거

--- src/pages/shop/hooks/useGetCartSummary.ts
+++
@@ -1,9 +0,0 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { getCartSummary } from '@/api/cart';
-
-export const useGetCartSummary = (orderableShopId: number) => {
-  return useSuspenseQuery({
-    queryKey: ['cartSummary', orderableShopId],
-    queryFn: () => getCartSummary({ orderableShopId: orderableShopId }),
-  });
-};
--- src/pages/shop/hooks/useGetShopDetail.ts
+++
@@ -1,9 +0,0 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { getShopDetailInfo } from '@/api/shop';
-
-export const useGetShopDetail = (orderableShopId: number) => {
-  return useSuspenseQuery({
-    queryKey: ['shopDetail', orderableShopId],
-    queryFn: () => getShopDetailInfo({ orderableShopId }),
-  });
-};
