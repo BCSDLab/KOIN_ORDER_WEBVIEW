ff36737a92af9ba373babe182f55306592de5ee7
이준영 <ff1451@gmail.com>
2025-07-14 16:00:16 +0900

    api 토큰 가져오는 것 테스트

--- src/api/cart/index.ts
+++ src/api/cart/index.ts
@@ -3,18 +3,16 @@ import { ShopMenuDetailResponse } from '../shop/entity';
 import { AddCartRequest, CartResponse, CartSummaryResponse, UpdateCartItemRequest } from '@/api/cart/entity';
 import { useTokenStore } from '@/stores/auth';
 
-function authHeader() {
-  const token = useTokenStore.getState().token;
-  return { Authorization: `Bearer ${token}` };
-}
-
-// const token = useTokenStore.getState().token;
+const token = useTokenStore.getState().token;
 
 // const token = localStorage.getItem('token'); // 개발용
 
 export const getCart = async (type: 'DELIVERY' | 'TAKE_OUT') => {
+  const token = useTokenStore.getState().token;
   return await apiClient.get<CartResponse>('cart', {
-    headers: authHeader(),
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
     params: {
       type: type,
     },
@@ -23,19 +21,25 @@ export const getCart = async (type: 'DELIVERY' | 'TAKE_OUT') => {
 
 export const validateCart = async () => {
   return await apiClient.get<CartResponse>('cart/validate', {
-    headers: authHeader(),
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 };
 
 export const resetCart = async () => {
   return await apiClient.delete('cart/reset', {
-    headers: authHeader(),
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 };
 
 export const deleteCartItem = async (cartMenuItemId: number) => {
   return await apiClient.delete(`cart/delete/${cartMenuItemId}`, {
-    headers: authHeader(),
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 };
 
@@ -47,32 +51,42 @@ export const updateCartItemQuantity = async ({
   quantity: number;
 }) => {
   return await apiClient.post(`cart/quantity/${cartMenuItemId}/${quantity}`, {
-    headers: authHeader(),
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 };
 
 export const addCart = async ({ menuInfo }: AddCartRequest) => {
   return await apiClient.post('cart/add', {
     body: menuInfo,
-    headers: authHeader(),
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 };
 
 export const getCartSummary = async ({ orderableShopId }: { orderableShopId: number }) => {
   return await apiClient.get<CartSummaryResponse>(`cart/summary/${orderableShopId}`, {
-    headers: authHeader(),
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 };
 
 export const getCartItemOptions = async (cartMenuItemId: number) => {
   return await apiClient.get<ShopMenuDetailResponse>(`cart/item/${cartMenuItemId}/edit`, {
-    headers: authHeader(),
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 };
 
 export const updateCartItemOptions = async (cartMenuItemId: number, body: UpdateCartItemRequest) => {
   return await apiClient.put(`cart/item/${cartMenuItemId}`, {
     body,
-    headers: authHeader(),
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 };
