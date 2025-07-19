69a550ce009f38fd72bdf6d6b54fdc5ff036998f
강성훈 <kshevl123@gmail.com>
2025-07-19 20:15:32 +0900

    feat: 로그인 유도 모달 추가

---
+++ src/pages/Shop/components/LoginRequiredModal.tsx
@@ -0,0 +1,45 @@
+import Button from '@/components/UI/Button';
+import Modal, { ModalContent } from '@/components/UI/CenterModal/Modal';
+
+interface LoginRequiredModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function LoginRequiredModal({ isOpen, onClose }: LoginRequiredModalProps) {
+  const redirectToLogin = () => {
+    // 로그인 페이지로..
+    onClose();
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalContent>
+        <div className="flex flex-col items-center gap-2 text-center leading-[160%] font-normal">
+          <div className="text-lg font-medium">
+            코인 주문을 이용하기 위해선 <br />
+            로그인이 필요해요.
+          </div>
+          <div className="text-sm text-neutral-500">
+            로그인 후 코인의 주문 기능을 <br />
+            이용해보세요!
+          </div>
+        </div>
+        <div className="flex w-full gap-2">
+          <Button
+            size="lg"
+            color="gray"
+            className="border-neutral-400 font-medium shadow-none"
+            fullWidth
+            onClick={onClose}
+          >
+            닫기
+          </Button>
+          <Button size="lg" color="primary" className="font-medium shadow-none" fullWidth onClick={redirectToLogin}>
+            로그인하기
+          </Button>
+        </div>
+      </ModalContent>
+    </Modal>
+  );
+}
