cfd937dd13b058b42d7dfc6b3f6ddfd63a182230
이준영 <ff1451@gmail.com>
2025-07-22 23:33:16 +0900

    feat: 테스트 페이지 변경

--- src/pages/Cart/test.tsx
+++ src/pages/Cart/test.tsx
@@ -1,134 +1,141 @@
-import clsx from 'clsx';
-import { useNavigate } from 'react-router-dom';
-import useCart from '../Payment/hooks/useCart';
-import BottomSheet from './components/BottomSheet';
-import CartItem from './components/CartItem';
-import PaymentAmount from './components/PaymentAmount';
-import EmptyCart from '@/assets/Cart/cart-icon.svg';
-import Plus from '@/assets/Cart/plus-icon.svg';
-import Info from '@/assets/Cart/primary-info-icon.svg';
-import PrimaryPlus from '@/assets/Cart/primary-plus-icon.svg';
-import RightArrow from '@/assets/Payment/arrow-go-icon.svg';
-import Button from '@/components/UI/Button';
-import { useOrderStore } from '@/stores/useOrderStore';
-import { closeWebviewPage } from '@/util/ts/bridge';
+// import clsx from 'clsx';
+// import { useNavigate } from 'react-router-dom';
+// import useCart from '../Payment/hooks/useCart';
+// import BottomSheet from './components/BottomSheet';
+// import CartItem from './components/CartItem';
+// import PaymentAmount from './components/PaymentAmount';
+// import EmptyCart from '@/assets/Cart/cart-icon.svg';
+// import Plus from '@/assets/Cart/plus-icon.svg';
+// import Info from '@/assets/Cart/primary-info-icon.svg';
+// import PrimaryPlus from '@/assets/Cart/primary-plus-icon.svg';
+// import RightArrow from '@/assets/Payment/arrow-go-icon.svg';
+// import Button from '@/components/UI/Button';
+// import { useOrderStore } from '@/stores/useOrderStore';
+// import { closeWebviewPage } from '@/util/ts/bridge';
 
-export default function Cart() {
-  const navigate = useNavigate();
-  const { orderType, setOrderType } = useOrderStore();
+import { getCookie } from '@/util/ts/cookie';
 
-  const { data: cartInfo } = useCart(orderType);
+// export default function Cart() {
+//   const navigate = useNavigate();
+//   const { orderType, setOrderType } = useOrderStore();
 
-  let infoMessage = '';
+//   const { data: cartInfo } = useCart(orderType);
 
-  if (!cartInfo.is_delivery_available && cartInfo.is_takeout_available) {
-    infoMessage = '이 가게는 포장주문만 가능해요';
-  } else if (cartInfo.is_delivery_available && !cartInfo.is_takeout_available) {
-    infoMessage = '이 가게는 배달주문만 가능해요';
-  }
+//   let infoMessage = '';
 
-  if (cartInfo.items.length === 0) {
-    return (
-      <div className="flex min-h-[calc(100vh-200px)] flex-col items-center justify-center">
-        <EmptyCart />
-        <div className="mt-1.5 text-[13px] leading-[160%] font-semibold text-neutral-700">장바구니가 비었어요</div>
-        <Button
-          startIcon={<Plus />}
-          color="gray"
-          className="mt-[14px] gap-2.5 border-0 py-[7px] pr-4 pl-[14px] text-[13px] font-bold"
-          onClick={() => closeWebviewPage()}
-        >
-          메뉴 추가
-        </Button>
-      </div>
-    );
-  }
+//   if (!cartInfo.is_delivery_available && cartInfo.is_takeout_available) {
+//     infoMessage = '이 가게는 포장주문만 가능해요';
+//   } else if (cartInfo.is_delivery_available && !cartInfo.is_takeout_available) {
+//     infoMessage = '이 가게는 배달주문만 가능해요';
+//   }
 
-  return (
-    <div className="mt-4 flex flex-col gap-6 px-6 pb-40">
-      <div>
-        <div className="shadow-1 flex w-full gap-1 rounded-[10px] bg-white p-1">
-          <Button
-            className={clsx(
-              'border-0 py-2 text-sm leading-[160%] font-medium shadow-none',
-              !cartInfo.is_delivery_available && 'bg-transparent text-neutral-300',
-            )}
-            fullWidth
-            color={orderType === 'DELIVERY' ? 'primary' : 'gray'}
-            state={cartInfo.is_delivery_available ? 'default' : 'disabled'}
-            onClick={() => setOrderType('DELIVERY')}
-          >
-            배달
-          </Button>
-          <Button
-            className={clsx(
-              'border-0 py-2 text-sm leading-[160%] font-medium shadow-none',
-              !cartInfo.is_takeout_available && 'bg-transparent text-neutral-300',
-            )}
-            fullWidth
-            color={orderType === 'TAKE_OUT' ? 'primary' : 'gray'}
-            state={cartInfo.is_takeout_available ? 'default' : 'disabled'}
-            onClick={() => setOrderType('TAKE_OUT')}
-          >
-            포장
-          </Button>
-        </div>
+//   if (cartInfo.items.length === 0) {
+//     return (
+//       <div className="flex min-h-[calc(100vh-200px)] flex-col items-center justify-center">
+//         <EmptyCart />
+//         <div className="mt-1.5 text-[13px] leading-[160%] font-semibold text-neutral-700">장바구니가 비었어요</div>
+//         <Button
+//           startIcon={<Plus />}
+//           color="gray"
+//           className="mt-[14px] gap-2.5 border-0 py-[7px] pr-4 pl-[14px] text-[13px] font-bold"
+//           onClick={() => closeWebviewPage()}
+//         >
+//           메뉴 추가
+//         </Button>
+//       </div>
+//     );
+//   }
 
-        {infoMessage && (
-          <div className="mt-1.5 flex items-center gap-2">
-            <Info />
-            <div className="text-primary-500 text-sm leading-[160%]">{infoMessage}</div>
-          </div>
-        )}
-      </div>
+//   return (
+//     <div className="mt-4 flex flex-col gap-6 px-6 pb-40">
+//       <div>
+//         <div className="shadow-1 flex w-full gap-1 rounded-[10px] bg-white p-1">
+//           <Button
+//             className={clsx(
+//               'border-0 py-2 text-sm leading-[160%] font-medium shadow-none',
+//               !cartInfo.is_delivery_available && 'bg-transparent text-neutral-300',
+//             )}
+//             fullWidth
+//             color={orderType === 'DELIVERY' ? 'primary' : 'gray'}
+//             state={cartInfo.is_delivery_available ? 'default' : 'disabled'}
+//             onClick={() => setOrderType('DELIVERY')}
+//           >
+//             배달
+//           </Button>
+//           <Button
+//             className={clsx(
+//               'border-0 py-2 text-sm leading-[160%] font-medium shadow-none',
+//               !cartInfo.is_takeout_available && 'bg-transparent text-neutral-300',
+//             )}
+//             fullWidth
+//             color={orderType === 'TAKE_OUT' ? 'primary' : 'gray'}
+//             state={cartInfo.is_takeout_available ? 'default' : 'disabled'}
+//             onClick={() => setOrderType('TAKE_OUT')}
+//           >
+//             포장
+//           </Button>
+//         </div>
 
-      <button className="flex items-center gap-1.5" onClick={() => navigate(`/shop/${cartInfo.orderable_shop_id}`)}>
-        <img src={cartInfo.shop_thumbnail_image_url} className="h-7.5 w-7.5 rounded-[5px]" />
-        <div className="text-lg leading-[160%] font-semibold">{cartInfo.shop_name}</div>
-        <RightArrow />
-      </button>
+//         {infoMessage && (
+//           <div className="mt-1.5 flex items-center gap-2">
+//             <Info />
+//             <div className="text-primary-500 text-sm leading-[160%]">{infoMessage}</div>
+//           </div>
+//         )}
+//       </div>
 
-      <div>
-        <div className="shadow-1 rounded-3xl">
-          {cartInfo.items.map((item, index) => (
-            <div
-              key={item.cart_menu_item_id}
-              className={clsx(
-                'bg-white px-6 py-3',
-                index === 0 && 'rounded-t-3xl',
-                index === cartInfo.items.length - 1 && 'rounded-b-3xl',
-                index !== cartInfo.items.length - 1 && 'border-b border-neutral-300',
-              )}
-            >
-              <CartItem shopId={cartInfo.orderable_shop_id} item={item} />
-            </div>
-          ))}
-        </div>
-        <Button
-          fullWidth
-          color="neutral"
-          startIcon={<PrimaryPlus />}
-          className="mt-3 gap-2.5 border-0 py-[11px] leading-[160%]"
-          onClick={() => navigate(`/shop/${cartInfo.orderable_shop_id}`)}
-        >
-          더 담으러 가기
-        </Button>
-      </div>
+//       <button className="flex items-center gap-1.5" onClick={() => navigate(`/shop/${cartInfo.orderable_shop_id}`)}>
+//         <img src={cartInfo.shop_thumbnail_image_url} className="h-7.5 w-7.5 rounded-[5px]" />
+//         <div className="text-lg leading-[160%] font-semibold">{cartInfo.shop_name}</div>
+//         <RightArrow />
+//       </button>
 
-      <PaymentAmount
-        orderType={orderType}
-        total_amount={cartInfo.total_amount}
-        item_total_amount={cartInfo.items_amount}
-        delivery_fee={cartInfo.delivery_fee}
-        final_payment_amount={cartInfo.final_payment_amount}
-      />
-      <BottomSheet
-        orderType={orderType}
-        itemCount={cartInfo.items.reduce((total, item) => total + item.quantity, 0)}
-        itemTotalAmount={cartInfo.items_amount}
-        totalAmount={cartInfo.total_amount}
-        minimumOrderAmount={cartInfo.shop_minimum_order_amount}
-      />
-    </div>
-  );
+//       <div>
+//         <div className="shadow-1 rounded-3xl">
+//           {cartInfo.items.map((item, index) => (
+//             <div
+//               key={item.cart_menu_item_id}
+//               className={clsx(
+//                 'bg-white px-6 py-3',
+//                 index === 0 && 'rounded-t-3xl',
+//                 index === cartInfo.items.length - 1 && 'rounded-b-3xl',
+//                 index !== cartInfo.items.length - 1 && 'border-b border-neutral-300',
+//               )}
+//             >
+//               <CartItem shopId={cartInfo.orderable_shop_id} item={item} />
+//             </div>
+//           ))}
+//         </div>
+//         <Button
+//           fullWidth
+//           color="neutral"
+//           startIcon={<PrimaryPlus />}
+//           className="mt-3 gap-2.5 border-0 py-[11px] leading-[160%]"
+//           onClick={() => navigate(`/shop/${cartInfo.orderable_shop_id}`)}
+//         >
+//           더 담으러 가기
+//         </Button>
+//       </div>
+
+//       <PaymentAmount
+//         orderType={orderType}
+//         total_amount={cartInfo.total_amount}
+//         item_total_amount={cartInfo.items_amount}
+//         delivery_fee={cartInfo.delivery_fee}
+//         final_payment_amount={cartInfo.final_payment_amount}
+//       />
+//       <BottomSheet
+//         orderType={orderType}
+//         itemCount={cartInfo.items.reduce((total, item) => total + item.quantity, 0)}
+//         itemTotalAmount={cartInfo.items_amount}
+//         totalAmount={cartInfo.total_amount}
+//         minimumOrderAmount={cartInfo.shop_minimum_order_amount}
+//       />
+//     </div>
+//   );
+// }
+export default function Test() {
+  const access = getCookie('AUTH_TOKEN_KEY');
+
+  return <div>액세스 토큰 : {access ? access : '토큰이 없습니다.'}</div>;
 }
