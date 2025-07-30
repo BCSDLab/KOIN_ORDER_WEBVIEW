1998e56b06e08055f82babece69ea637e8363d63
이준영 <ff1451@gmail.com>
2025-07-30 09:17:27 +0900

    refactor: 제출 버튼 문구 변경 및 전화번호 포매팅 함수 유틸 분리

---
+++ src/util/ts/formatPhoneNumber.ts
@@ -0,0 +1,13 @@
+export default function formatPhoneNumber(phone: string) {
+  const digits = phone.replace(/\D/g, '');
+
+  if (digits.length <= 3) {
+    return digits;
+  } else if (digits.length <= 7) {
+    return `${digits.slice(0, 3)}-${digits.slice(3)}`;
+  } else if (digits.length <= 11) {
+    return `${digits.slice(0, 3)}-${digits.slice(3, 7)}-${digits.slice(7, 11)}`;
+  } else {
+    return `${digits.slice(0, 3)}-${digits.slice(3, 7)}-${digits.slice(7, 11)}`;
+  }
+}
