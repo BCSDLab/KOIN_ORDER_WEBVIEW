3acd0c89b58810a30fc50d138554d9a4d78becb5
강성훈 <kshevl123@gmail.com>
2025-07-30 17:15:37 +0900

    chore: 주변상점 type 추가

--- src/api/shop/entity.ts
+++ src/api/shop/entity.ts
@@ -33,9 +33,77 @@ export interface ShopDetailInfoResponse {
   phone: string;
   introduction: string | null;
   notice: string | null;
-  delivery_tips: DeliveryTips[];
+  delivery_tips: DeliveryTips[] | [];
   owner_info: OwnerInfo;
-  origins: OriginInfo[];
+  origins: OriginInfo[] | [];
+}
+
+export interface Time {
+  day_of_week: Day;
+  closed: boolean;
+  open_time: string | null;
+  close_time: string | null;
+}
+
+export interface CategoryGroup {
+  id: number;
+  name: string;
+}
+
+export interface UnOrderableShopDetailInfoResponse {
+  address: string;
+  delivery: boolean;
+  delivery_price: number;
+  description: string;
+  id: number;
+  image_urls: string[];
+  menu_categories: MenuGroup[];
+  name: string;
+  open: Time[];
+  pay_bank: boolean;
+  pay_card: boolean;
+  phone: string;
+  main_category_id: number;
+  shop_categories: CategoryGroup[];
+  updated_at: string;
+  is_event: boolean;
+  bank: string | null;
+  account_number: string | null;
+}
+
+interface Rating {
+  '1': number;
+  '2': number;
+  '3': number;
+  '4': number;
+  '5': number;
+}
+
+interface ReviewStatistics {
+  average_rating: number;
+  ratings: Rating;
+}
+
+interface Review {
+  review_id: number;
+  rating: number;
+  nick_name: string;
+  content: string;
+  image_urls: string[];
+  menu_names: string[];
+  is_mine: boolean;
+  is_modified: boolean;
+  is_reported: boolean;
+  created_at: string;
+}
+
+export interface UnOrderableShopReviewsResponse {
+  total_count: number;
+  current_count: number;
+  total_page: number;
+  current_page: number;
+  statistics: ReviewStatistics;
+  reviews: Review[];
 }
 
 export interface ShopDeliveryInfoResponse {
@@ -43,9 +111,13 @@ export interface ShopDeliveryInfoResponse {
   off_campus_delivery: boolean;
 }
 
-export interface ShopInfoParams {
+export interface OrderableShopInfoParams {
   orderableShopId: number;
   orderableShopMenuId?: number;
+}
+
+export interface UnOrderableShopInfoParams {
+  UnOrderableShopId: number;
 }
 
 export interface Price {
@@ -55,7 +127,7 @@ export interface Price {
   is_selected: boolean;
 }
 
-interface Menu {
+export interface Menu {
   id: number;
   name: string;
   description: string;
@@ -68,6 +140,34 @@ export interface ShopInfoResponse {
   menu_group_id: number;
   menu_group_name: string;
   menus: Menu[];
+}
+
+interface UnOrderableOptions {
+  option: string;
+  price: number;
+}
+
+export interface UnOrderableMenu {
+  id: number;
+  name: string;
+  is_hidden: boolean;
+  is_single: boolean;
+  single_price: number;
+  option_prices: UnOrderableOptions[];
+  description: string | null;
+  image_urls: string[];
+}
+
+interface UnOrderableMenuCategory {
+  id: number;
+  name: string;
+  menus: UnOrderableMenu[];
+}
+
+export interface UnOrderableShopMenusResponse {
+  count: number;
+  menu_categories: UnOrderableMenuCategory[];
+  updated_at: string;
 }
 
 interface Image {
