e22d33908129385640673f2ed4323916efc0a5bb
강성훈 <kshevl123@gmail.com>
2025-07-30 17:23:29 +0900

    feat: 상점 세부정보 컴포넌트 추가

---
+++ src/pages/Shop/ShopDetail/ShopDetailView.tsx
@@ -0,0 +1,18 @@
+import { useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import OrderableShopDetail from './OrderableShopDetail';
+import UnOrderableShopDetail from './UnOrderableShopDetail';
+
+export default function ShopDetailView() {
+  const { state } = useLocation();
+  const { id } = useParams();
+  if (!id) {
+    throw new Error('Shop ID is required');
+  }
+
+  if (!state) {
+    throw new Error('State is required');
+  }
+
+  return <>{state.isOrderable ? <OrderableShopDetail shopId={id} /> : <UnOrderableShopDetail shopId={id} />}</>;
+}
