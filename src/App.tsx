ec02bb0fb32e88c3ab9d8a829163c2c8c14c8791
이준영 <ff1451@gmail.com>
2025-08-04 07:33:37 +0900

    feat : 브릿지 테스트 과정에서 에러 확인을 위한 errorboundary 임시 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Cart from './pages/Cart';
 import TestPage from './pages/Cart/test';
 import DeliveryOutside from './pages/Delivery/Outside';
 import OrderCancel from './pages/OrderFinish/OrderCancel';
-import Shop from './pages/Shop';
 import MenuDetail from './pages/Shop/MenuDetail';
 import ShopDetail from './pages/Shop/ShopDetail';
+import ShopRouteWrapper from './pages/Shop/ShopWrapper';
 import AppLayout from '@/components/Layout';
 import Campus from '@/pages/Delivery/Campus';
 import DetailAddress from '@/pages/Delivery/Outside/DetailAddress';
@@ -22,7 +22,7 @@ export default function App() {
       <Routes>
         <Route path="test" element={<TestPage />} />
 
-        <Route path="shop/:shopId" element={<Shop />} />
+        <Route path="shop/:shopId" element={<ShopRouteWrapper />} />
         <Route path="shop/:shopId/menus/:menuId" element={<MenuDetail />} />
         <Route element={<AppLayout />}>
           <Route path="shop-detail/:id" element={<ShopDetail />} />
