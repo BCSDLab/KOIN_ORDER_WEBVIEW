3c7c2dab0a62a22f8a55e685e315186701a11143
강성훈 <kshevl123@gmail.com>
2025-08-03 16:33:34 +0900

    refactor: tanStackQuery로 데이터 가공 처리

--- src/pages/Shop/components/UnOrderableComponent.tsx
+++ src/pages/Shop/components/UnOrderableComponent.tsx
@@ -30,66 +30,27 @@ export default function UnOrderableComponent({ totalQuantity }: UnOrderableCompo
   const { data: unOrderableShopMenuGroups } = useGetUnOrderableShopMenuGroups(Number(shopId));
   const { data: unOrderableShopMenus } = useGetUnOrderableShopMenus(Number(shopId));
 
-  const carouselImages =
-    shopInfoSummary.image_urls?.map((url, index) => ({
-      image_url: url,
-      is_thumbnail: index === 1 ? true : false,
-    })) || [];
-
   const shopInfoSummaryData = {
-    shop_id: shopInfoSummary.id,
+    ...shopInfoSummary,
     orderable_shop_id: Number(shopId),
-    name: shopInfoSummary.name,
-    introduction: shopInfoSummary.description,
-    pay_card: shopInfoSummary.pay_card,
-    pqy_bank: shopInfoSummary.pay_bank,
-    is_delivery_available: false,
-    is_takeout_available: false,
-    minimum_order_amount: 0,
-    rating_average: shopReviews.statistics.average_rating,
-    review_count: shopReviews.total_count,
-    minimum_delivery_tip: 0,
-    maximum_delivery_tip: 0,
-    images: carouselImages,
+    ...shopReviews,
   };
-
-  const shopMenuGroups = {
-    count: unOrderableShopMenuGroups.menu_categories.length,
-    menu_groups: unOrderableShopMenuGroups.menu_categories.map((category) => ({
-      id: category.id,
-      name: category.name,
-    })),
-  };
-
-  const shopMenus = unOrderableShopMenus.menu_categories.map((category) => ({
-    menu_group_id: category.id,
-    menu_group_name: category.name,
-    menus: category.menus.map((menu) => ({
-      id: menu.id,
-      name: menu.name,
-      description: menu.description ?? '',
-      thumbnail_image: menu.image_urls?.[0] || '',
-      is_sold_out: false,
-      prices: (menu.option_prices || []).map((price, index) => ({
-        id: index,
-        name: price.option,
-        price: price.price,
-        is_selected: false,
-      })),
-    })),
-  }));
 
   return (
     <>
       <Header name={shopInfoSummary.name} targetRef={targetRef} cartItemCount={totalQuantity} />
-      <ImageCarousel images={carouselImages} targetRef={targetRef} />
+      <ImageCarousel images={shopInfoSummaryData.images} targetRef={targetRef} />
       <ShopSummary id={shopId} shopInfoSummary={shopInfoSummaryData} isOrderable={false} />
-      <ShopMenuGroups selectedMenu={selectedMenu} onSelect={handleScrollTo} shopMenuGroups={shopMenuGroups} />
+      <ShopMenuGroups
+        selectedMenu={selectedMenu}
+        onSelect={handleScrollTo}
+        shopMenuGroups={unOrderableShopMenuGroups}
+      />
       <ShopMenus
         menuGroupRefs={menuGroupRefs}
         handleChangeMenu={handleChangeMenu}
         isAutoScrolling={isAutoScrolling}
-        shopMenus={shopMenus}
+        shopMenus={unOrderableShopMenus}
         isOrderable={false}
       />
     </>
--- src/pages/Shop/hooks/useGetShopDetail.ts
+++ src/pages/Shop/hooks/useGetShopDetail.ts
@@ -1,9 +1,36 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { getShopDetailInfo } from '@/api/shop';
+import { getShopDetailInfo, getUnOrderableShopInfo } from '@/api/shop';
 
 export const useGetShopDetail = (orderableShopId: number) => {
   return useSuspenseQuery({
     queryKey: ['shopDetail', orderableShopId],
     queryFn: () => getShopDetailInfo({ orderableShopId }),
+  });
+};
+
+export const useGetUnOrderableShopDetail = (UnOrderableShopId: number) => {
+  return useSuspenseQuery({
+    queryKey: ['unOrderableShopDetail', UnOrderableShopId],
+    queryFn: () => getUnOrderableShopInfo({ UnOrderableShopId }),
+    select: (data) => ({
+      shop_id: data.id,
+      orderable_shop_id: 0,
+      name: data.name,
+      address: data.address,
+      open_time: data.open[0].open_time ?? '',
+      close_time: data.open[0].close_time ?? '',
+      closed_days: data.open.filter((time) => time.closed).map((time) => time.day_of_week),
+      phone: data.phone,
+      introduction: data.description as string | null,
+      notice: '',
+      delivery_tips: [],
+      owner_info: {
+        name: '',
+        shop_name: '',
+        address: '',
+        company_registration_number: '',
+      },
+      origins: [],
+    }),
   });
 };
