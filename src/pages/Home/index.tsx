f65d649bf288e14e51527977e143de1ff08ebf1b
JeongWon-CHO <ahin0312@naver.com>
2025-09-16 17:47:00 +0900

    refactor: 여백 배경 채움

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,12 +4,12 @@ import SearchBar from './components/SearchBar';
 
 export default function Home() {
   return (
-    <div className="flex w-screen flex-col bg-[#f2f2f2]">
+    <div className="flex min-h-screen w-full flex-col bg-[#f2f2f2]">
       <div className="flex justify-center">
         <SearchBar />
       </div>
 
-      <div className="flex justify-center pt-15 pb-30">
+      <div className="flex flex-1 justify-center pt-15 pb-30">
         <OrderList />
       </div>
 
