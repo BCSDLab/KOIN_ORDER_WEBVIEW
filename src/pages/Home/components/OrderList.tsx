8f0ee1839b7daa7559eff65099f3a7ee2524a7bb
JeongWon-CHO <ahin0312@naver.com>
2025-09-16 14:40:10 +0900

    feat: filter UI 추가

--- src/pages/Home/components/OrderList.tsx
+++ src/pages/Home/components/OrderList.tsx
@@ -1,3 +1,7 @@
+import MenuCard from './MenuCard';
+import Delivery from '@/assets/Home/delivery-icon.svg';
+import DownArrow from '@/assets/Payment/arrow-go-icon.svg';
+import { useOrderableShops } from '@/pages/Home/hooks/useOrderableShops.ts';
 import { useStoreCategories } from '@/pages/Home/hooks/useStoreCategories.ts';
 
 interface Category {
@@ -12,9 +16,14 @@ export default function OrderList() {
     ...category,
   }));
 
+  const { data: openShops } = useOrderableShops({
+    filter: ['IS_OPEN'],
+  });
+  console.log(openShops);
+
   return (
-    <div className="flex w-full flex-col items-center justify-center">
-      <div className="overflow: visible ml-6 flex w-[calc(100%-24px)] snap-x snap-mandatory gap-6 overflow-x-auto pb-4 [-ms-overflow-style:none] [scrollbar-width:none] min-[960px]:ml-0 min-[960px]:snap-none min-[960px]:justify-center [&::-webkit-scrollbar]:hidden">
+    <div className="flex w-full flex-col items-center justify-center gap-4">
+      <div className="ml-6 flex w-[calc(100%-24px)] snap-x snap-mandatory gap-6 overflow-x-auto pb-4 min-[960px]:ml-0 min-[960px]:snap-none min-[960px]:justify-center [@media(pointer:coarse)]:[-ms-overflow-style:none] [@media(pointer:coarse)]:[scrollbar-width:none] [@media(pointer:coarse)]:[&::-webkit-scrollbar]:hidden [@media(pointer:fine)]:[scrollbar-width:thin] [@media(pointer:fine)]:[&::-webkit-scrollbar]:h-2 [@media(pointer:fine)]:[&::-webkit-scrollbar]:w-2 [@media(pointer:fine)]:[&::-webkit-scrollbar-thumb]:rounded-full [@media(pointer:fine)]:[&::-webkit-scrollbar-thumb]:bg-neutral-300 [@media(pointer:fine)]:[&::-webkit-scrollbar-track]:bg-transparent">
         {categoriesWithAll.map((category) => (
           <button
             key={category.id}
@@ -28,8 +37,48 @@ export default function OrderList() {
         ))}
       </div>
 
-      <div className="mt-40 mb-40 text-center">광고배너</div>
-      <div className="text-center">메뉴 리스트</div>
+      {/* 활성화 효과 넣고 필터 로직까지 한 다음에 분리 ㄱ*/}
+      <div className="flex w-full pr-4 min-[600px]:justify-center">
+        <div className="flex w-full min-[600px]:flex-wrap min-[600px]:justify-center min-[600px]:gap-2">
+          <button className="mr-4 ml-4 inline-flex shrink-0 items-center justify-center gap-[6px] rounded-3xl bg-white px-2 py-[6px] text-[14px] leading-none text-gray-400 shadow-[0_1px_1px_0_rgba(0,0,0,0.02),_0_2px_4px_0_rgba(0,0,0,0.04)] min-[600px]:mr-0 min-[600px]:ml-0">
+            기본순
+            <DownArrow className="h-4 w-4 rotate-90" />
+          </button>
+
+          <div className="flex flex-1 snap-x snap-mandatory gap-2 overflow-x-auto min-[600px]:flex-initial min-[600px]:snap-none min-[600px]:overflow-visible [@media(pointer:coarse)]:[-ms-overflow-style:none] [@media(pointer:coarse)]:[scrollbar-width:none] [@media(pointer:coarse)]:[&::-webkit-scrollbar]:hidden [@media(pointer:fine)]:[scrollbar-width:thin] [@media(pointer:fine)]:[&::-webkit-scrollbar]:h-2 [@media(pointer:fine)]:[&::-webkit-scrollbar]:w-2 [@media(pointer:fine)]:[&::-webkit-scrollbar-thumb]:rounded-full [@media(pointer:fine)]:[&::-webkit-scrollbar-thumb]:bg-neutral-300 [@media(pointer:fine)]:[&::-webkit-scrollbar-track]:bg-transparent">
+            <button className="flex shrink-0 snap-start items-center justify-center gap-[6px] rounded-3xl bg-white px-2 py-[6px] text-[14px] text-gray-400 shadow-[0_1px_1px_0_rgba(0,0,0,0.02),_0_2px_4px_0_rgba(0,0,0,0.04)]">
+              <Delivery fill="#cacaca" />
+              영업중
+            </button>
+
+            <button className="flex shrink-0 snap-start items-center justify-center gap-[6px] rounded-3xl bg-white px-2 py-[6px] text-[14px] text-gray-400 shadow-[0_1px_1px_0_rgba(0,0,0,0.02),_0_2px_4px_0_rgba(0,0,0,0.04)]">
+              <Delivery fill="#cacaca" />
+              배달가능
+            </button>
+
+            <button className="flex shrink-0 snap-start items-center justify-center gap-[6px] rounded-3xl bg-white px-2 py-[6px] text-[14px] text-gray-400 shadow-[0_1px_1px_0_rgba(0,0,0,0.02),_0_2px_4px_0_rgba(0,0,0,0.04)]">
+              <Delivery fill="#cacaca" />
+              포장가능
+            </button>
+
+            <button className="flex shrink-0 snap-start items-center justify-center gap-[6px] rounded-3xl bg-white px-2 py-[6px] text-[14px] text-gray-400 shadow-[0_1px_1px_0_rgba(0,0,0,0.02),_0_2px_4px_0_rgba(0,0,0,0.04)]">
+              <Delivery fill="#cacaca" />
+              배달팁무료
+            </button>
+
+            <button className="flex shrink-0 snap-start items-center justify-center gap-[6px] rounded-3xl bg-white px-2 py-[6px] text-[14px] text-gray-400 shadow-[0_1px_1px_0_rgba(0,0,0,0.02),_0_2px_4px_0_rgba(0,0,0,0.04)]">
+              <Delivery fill="#cacaca" />
+              최소주문금액
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-20 mb-20 text-center">광고배너</div>
+      <div className="flex flex-col gap-6">
+        <MenuCard />
+        <MenuCard />
+      </div>
     </div>
   );
 }
