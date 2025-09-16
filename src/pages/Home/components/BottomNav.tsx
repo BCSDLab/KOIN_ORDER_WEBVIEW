d8aa1eddf4aa105960395cd5ea757a2983f9976a
JeongWon-CHO <ahin0312@naver.com>
2025-09-16 16:52:34 +0900

    refactor: 버튼 크기 변경 및 폰트 사이즈 변경

--- src/pages/Home/components/BottomNav.tsx
+++ src/pages/Home/components/BottomNav.tsx
@@ -4,17 +4,17 @@ import PaperIcon from '/src/assets/Home/paper-icon.svg';
 
 export default function BottomNav() {
   return (
-    <div className="fixed right-0 bottom-0 left-0 z-40 flex items-center justify-between bg-white px-12 py-4">
-      <button className="flex flex-col gap-0.5">
+    <div className="fixed right-0 bottom-0 left-0 z-40 flex items-center justify-between bg-white px-12 py-4 text-[12px]">
+      <button className="flex w-[42px] flex-col items-center gap-0.5">
         <HomeIcon fill="black" />홈
       </button>
 
-      <button className="flex flex-col items-center gap-0.5">
+      <button className="flex w-[42px] flex-col items-center gap-0.5">
         <StoreIcon fill="black" />
         주변상점
       </button>
 
-      <button className="flex flex-col items-center gap-0.5">
+      <button className="flex w-[42px] flex-col items-center gap-0.5">
         <PaperIcon fill="black" />
         주문내역
       </button>
