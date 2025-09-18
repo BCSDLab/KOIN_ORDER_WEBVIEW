9ecc05a0ad31efe50cc4c49a867fb7bbcd74f6b3
JeongWon-CHO <ahin0312@naver.com>
2025-09-18 22:25:02 +0900

    feat: 하단 네비바 네비게이터 추가

--- src/pages/Home/components/BottomNav.tsx
+++ src/pages/Home/components/BottomNav.tsx
@@ -1,11 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import HomeIcon from '/src/assets/Home/home-icon.svg';
 import StoreIcon from '/src/assets/Home/store-icon.svg';
 import PaperIcon from '/src/assets/Home/paper-icon.svg';
 
 export default function BottomNav() {
+  const navigate = useNavigate();
+
   return (
     <div className="fixed right-0 bottom-0 left-0 z-40 flex items-center justify-between bg-white px-12 py-4 text-[12px]">
-      <button className="flex w-[42px] flex-col items-center gap-0.5">
+      <button onClick={() => navigate(`/home`)} className="flex w-[42px] flex-col items-center gap-0.5">
         <HomeIcon fill="black" />홈
       </button>
 
