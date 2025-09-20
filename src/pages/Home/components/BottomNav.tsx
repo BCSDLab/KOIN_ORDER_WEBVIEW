aadc9722ffd9cedf10ba31fedb568559a73aa720
JeongWon-CHO <ahin0312@naver.com>
2025-09-20 16:32:32 +0900

    refactor: 아이콘 색상 수정

--- src/pages/Home/components/BottomNav.tsx
+++ src/pages/Home/components/BottomNav.tsx
@@ -1,24 +1,42 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import HomeIcon from '/src/assets/Home/home-icon.svg';
 import StoreIcon from '/src/assets/Home/store-icon.svg';
 import PaperIcon from '/src/assets/Home/paper-icon.svg';
 
 export default function BottomNav() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const isActive = (path: string) => {
+    return location.pathname.startsWith(path);
+  };
 
   return (
     <div className="fixed right-0 bottom-0 left-0 z-40 flex items-center justify-between bg-white px-12 py-4 text-[12px]">
-      <button onClick={() => navigate(`/home`)} className="flex w-[42px] flex-col items-center gap-0.5">
-        <HomeIcon fill="black" />홈
+      <button
+        onClick={() => navigate(`/home`)}
+        className={`flex w-[42px] flex-col items-center gap-0.5 ${
+          isActive('/home') ? 'text-primary-500' : 'text-neutral-500'
+        }`}
+      >
+        <HomeIcon className={isActive('/home') ? 'fill-primary-500' : 'fill-neutral-300'} />홈
       </button>
 
-      <button className="flex w-[42px] flex-col items-center gap-0.5">
-        <StoreIcon fill="black" />
+      <button
+        className={`flex w-[42px] flex-col items-center gap-0.5 ${
+          isActive('/stores') ? 'text-primary-500' : 'text-neutral-500'
+        }`}
+      >
+        <StoreIcon className={isActive('/stores') ? 'fill-primary-500' : 'fill-neutral-300'} />
         주변상점
       </button>
 
-      <button className="flex w-[42px] flex-col items-center gap-0.5">
-        <PaperIcon fill="black" />
+      <button
+        className={`flex w-[42px] flex-col items-center gap-0.5 ${
+          isActive('/orders') ? 'text-primary-500' : 'text-neutral-500'
+        }`}
+      >
+        <PaperIcon className={isActive('/orders') ? 'fill-primary-500' : 'fill-neutral-300'} />
         주문내역
       </button>
     </div>
