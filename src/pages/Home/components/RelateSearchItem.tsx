b70d59e622753e435e1cdce13740ee42a7254c6b
jaeik <lji0823@naver.com>
2025-09-21 03:26:13 +0900

    fix: 이벤트 타입 명시 & 인터페이스 구조 변경

--- src/pages/Home/components/RelateSearchItem.tsx
+++ src/pages/Home/components/RelateSearchItem.tsx
@@ -3,27 +3,25 @@ import MenuIcon from '/src/assets/Home/menu-icon.svg';
 import NavigateStoreIcon from '/src/assets/Home/navigate-store-icon.svg';
 
 interface RelatedSearchItemProps {
-  url: string | null;
+  tag: 'store' | 'menu';
   content: string;
-  onClick: () => void;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function RelateSearchItem({ url, content, onClick }: RelatedSearchItemProps) {
+export default function RelateSearchItem({ tag, content, onClick }: RelatedSearchItemProps) {
   return (
     <button
       type="button"
       onClick={onClick}
-      className="flex h-12 w-[342px] items-center justify-between border-b border-[#eee] p-2 sm:w-[936px]"
+      className="flex h-12 w-[342px] items-center justify-between border-b border-[#eee] p-2 max-[341px]:w-full lg:w-[936px]"
     >
       <div className="flex items-center gap-1">
-        <div className="h-4 w-4">{url ? <StoreIcon /> : <MenuIcon />}</div>
-        <p className="text-[10px] text-[#727272]">{content}</p>
+        <div className="h-4 w-4">{tag === 'store' ? <StoreIcon /> : <MenuIcon />}</div>
+        <p className="text-[10px] text-neutral-500">{content}</p>
       </div>
-      {url && (
-        <div className="h-4 w-4">
-          <NavigateStoreIcon />
-        </div>
-      )}
+      <div className="h-4 w-4">
+        <NavigateStoreIcon />
+      </div>
     </button>
   );
 }
