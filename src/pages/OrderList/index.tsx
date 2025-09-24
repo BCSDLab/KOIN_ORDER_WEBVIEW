63227eeaa1f3587e52c5fabe1ae0ae79048ef4e6
이준영 <ff1451@gmail.com>
2025-09-24 18:41:32 +0900

    feat: 빈 카드 및 무한스크롤 구현

--- src/pages/OrderList/index.tsx
+++ src/pages/OrderList/index.tsx
@@ -1,36 +1,62 @@
 import { useState } from 'react';
+import EmptyCard from './components/EmptyCard';
 import OrderCardList from './components/OrderCardList';
 import OrderHistoryTab from './components/OrderHistoryTab';
+import PreparingCardList from './components/PreparingCardList';
+import useInProgressOrder from './hooks/useInProgressOrder';
 import { useOrderHistory } from './hooks/useOrderHistory';
-import SleepingIcon from '@/assets/OrderHistory/sleeping-icon.svg';
-
-function EmptyOrders() {
-  return (
-    <div className="text-primary-500 flex flex-col items-center justify-center gap-4 py-30 text-lg font-semibold">
-      <SleepingIcon />
-      <div>주문 내역이 없어요</div>
-    </div>
-  );
-}
 
 export default function OrderList() {
   const [tab, setTab] = useState<'past' | 'preparing'>('past');
+  const isPast = tab === 'past';
 
-  const { data: orders } = useOrderHistory({
+  const {
+    data: orders = [],
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useOrderHistory({
     page: 1,
     limit: 10,
-    period: 'NONE',
-    status: 'NONE',
-    type: 'NONE',
+    period: 'NONE' as const,
+    status: 'NONE' as const,
+    type: 'NONE' as const,
     query: '',
   });
+  const { data: PreparingOrders } = useInProgressOrder();
 
-  if (!orders) return null;
+  if (isPast && orders.length === 0) {
+    return (
+      <>
+        <OrderHistoryTab activeTab={tab} onTabChange={setTab} />
+        <EmptyCard activeTab="past" setActiveTab={setTab} />
+      </>
+    );
+  }
+
+  if (!isPast && PreparingOrders.length === 0) {
+    return (
+      <>
+        <OrderHistoryTab activeTab={tab} onTabChange={setTab} />
+        <EmptyCard activeTab="preparing" setActiveTab={setTab} />
+      </>
+    );
+  }
 
   return (
     <>
       <OrderHistoryTab activeTab={tab} onTabChange={setTab} />
-      {tab === 'past' && (orders.length === 0 ? <EmptyOrders /> : <OrderCardList orders={orders} />)}
+
+      {isPast ? (
+        <OrderCardList
+          orders={orders}
+          hasNextPage={hasNextPage}
+          isFetchingNextPage={isFetchingNextPage}
+          onLoadMore={() => fetchNextPage()}
+        />
+      ) : (
+        <PreparingCardList orders={PreparingOrders} />
+      )}
     </>
   );
 }
