5481b0f7619f3de7a89a2de49dd3fe65a02c01a9
jaeik <lji0823@naver.com>
2025-09-29 20:17:01 +0900

    fix: 불필요한 params 코드 삭제

--- src/pages/Home/components/SearchBar.tsx
+++ src/pages/Home/components/SearchBar.tsx
@@ -1,15 +1,13 @@
 import SearchBarModal from './SearchBarModal';
 import SearchBox from './SearchBox';
 import useBooleanState from '@/util/hooks/useBooleanState';
-import useParamsHandler from '@/util/hooks/useParamsHandler';
 
 export default function SearchBar() {
   const [isModalOpen, openModal, closeModal] = useBooleanState(false);
-  const { searchParams } = useParamsHandler();
 
   return (
     <>
-      <SearchBox mode="button" text={searchParams.get('storeName') || '검색어를 입력해주세요.'} onClick={openModal} />
+      <SearchBox mode="button" text="검색어를 입력해주세요." onClick={openModal} />
       {isModalOpen && <SearchBarModal onClose={closeModal} />}
     </>
   );
