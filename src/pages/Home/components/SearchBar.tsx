d460f05810beeea44e633111214f92286aa4a3a2
jaeik <lji0823@naver.com>
2025-10-01 01:52:03 +0900

    fix: 검색창 타입 변경 & 디바운스 처리

--- src/pages/Home/components/SearchBar.tsx
+++ src/pages/Home/components/SearchBar.tsx
@@ -1,14 +1,51 @@
+import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import SearchBarModal from './SearchBarModal';
-import SearchBox from './SearchBox';
+import { getRelateSearch } from '@/api/shop';
+import { RelatedSearchResponse } from '@/api/shop/entity';
+import SearchIconGray from '@/assets/Main/search-icon-gray.svg';
 import useBooleanState from '@/util/hooks/useBooleanState';
+import useDebouncedCallback from '@/util/hooks/useDebounce';
 
 export default function SearchBar() {
   const [isModalOpen, openModal, closeModal] = useBooleanState(false);
 
+  const [keyword, setKeyword] = useState('');
+
+  const setDebounced = useDebouncedCallback((value: string) => {
+    setKeyword(value.trim());
+  }, 200);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDebounced(e.target.value);
+  };
+
+  const { data: relatedSearchItems } = useQuery<RelatedSearchResponse>({
+    queryKey: ['relateSearch', keyword],
+    queryFn: () => getRelateSearch({ keyword }),
+    enabled: keyword.length > 0,
+  });
+
   return (
     <>
-      <SearchBox mode="button" text="검색어를 입력해주세요." onClick={openModal} />
-      {isModalOpen && <SearchBarModal onClose={closeModal} />}
+      <div
+        className={
+          'shadow-1 mt-2 flex h-10 w-[342px] items-center gap-2 rounded-2xl bg-white px-3 py-2 max-[341px]:w-full lg:w-[936px]'
+        }
+      >
+        <span className="flex h-6 w-6 items-center justify-center p-1">
+          <SearchIconGray />
+        </span>
+        <input
+          type="text"
+          placeholder="검색어를 입력해주세요."
+          autoComplete="off"
+          onClick={openModal}
+          className="w-full text-[14px] leading-[160%] placeholder-neutral-500 focus:outline-none"
+          onChange={onChange}
+        />
+      </div>
+      {isModalOpen && <SearchBarModal onClose={closeModal} relatedSearchItems={relatedSearchItems} />}
     </>
   );
 }
