6be973bd4c0e9c624ae1782159b0d940e1c3d556
JeongWon-CHO <ahin0312@naver.com>
2025-10-05 13:21:50 +0900

    refactor: 머지 충돌 수정

--- src/components/Layout/Searchbar/index.tsx
+++ src/components/Layout/Searchbar/index.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import SearchBar from '@/pages/Home/components/SearchBar';
+import Search from '@/pages/Search';
 
 export default function Searchbar() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-[#f8f8fa]">
       <div className="flex justify-center py-2">
-        <SearchBar />
+        <Search />
       </div>
       <div className="flex-1">
         <Outlet />
