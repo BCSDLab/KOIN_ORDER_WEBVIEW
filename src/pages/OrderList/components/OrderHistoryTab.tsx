be77cd90639b0a639ba218e4ac386c3696537ce6
jaeik <lji0823@naver.com>
2025-10-27 16:02:31 +0900

    fix: 불필요한 코드 삭제

--- src/pages/OrderList/components/OrderHistoryTab.tsx
+++ src/pages/OrderList/components/OrderHistoryTab.tsx
@@ -7,7 +7,7 @@ interface TabProps {
 
 export default function Tab({ activeTab, onTabChange }: TabProps) {
   return (
-    <div className="fixed top-15 right-0 left-0 z-110 flex w-full border-b border-b-neutral-400 bg-[#F8F8FA] font-semibold text-neutral-500">
+    <div className="fixed top-15 z-110 flex w-full border-b border-b-neutral-400 bg-[#F8F8FA] font-semibold text-neutral-500">
       <button
         className={clsx('flex-1 py-3', activeTab === 'past' && 'text-primary-500 border-b-primary-500 border-b-2')}
         onClick={() => onTabChange('past')}
