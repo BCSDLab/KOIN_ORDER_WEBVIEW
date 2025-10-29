0ba86c86f0f4854a2bb4f5c55a994943f3c6c242
JeongWon-CHO <ahin0312@naver.com>
2025-10-29 22:10:58 +0900

    test: 에러 페이지에 대한 테스트 방식 변경

--- tests/routes.e2e.ts
+++ tests/routes.e2e.ts
@@ -9,9 +9,8 @@ for (const route of routes) {
 
     await expect(page).toHaveTitle(/.+/); // 제목이 있는지(페이지가 제대로 로드 되었는지 확인)
 
-    // 에러 페이지가 아닌지 확인 (404 또는 Error 텍스트가 있는지 확인)
-    const bodyText = await page.textContent('body');
-    expect(bodyText).not.toContain('404');
-    expect(bodyText).not.toContain('Error');
+    // 에러 페이지가 아닌 지 확인(흰 화면 감지)
+    const root = page.locator('#root');
+    await expect(root).not.toBeEmpty();
   });
 }
