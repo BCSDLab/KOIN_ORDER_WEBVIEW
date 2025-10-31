f33e7f24dfce22db284918ec951a346975c5ce1f
이준영 <ff1451@gmail.com>
2025-10-31 21:45:41 +0900

    feat: 검색관련 로깅 누락 파일 추가

--- src/pages/Search/components/RelateSearchItem.tsx
+++ src/pages/Search/components/RelateSearchItem.tsx
@@ -1,20 +1,42 @@
 import StoreIcon from '/src/assets/Home/stores-icon.svg';
 import MenuIcon from '/src/assets/Home/menu-icon.svg';
 import NavigateStoreIcon from '/src/assets/Home/navigate-store-icon.svg';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
+import useLogger from '@/util/hooks/analytics/useLogger';
+import { getCategoryNameById } from '@/constants/shopCategories';
 
 interface RelatedSearchItemProps {
   tag: 'store' | 'menu';
-  content: string;
   to: string;
+  shop_name: string;
+  menu_name?: string;
 }
 
-export default function RelateSearchItem({ tag, content, to }: RelatedSearchItemProps) {
+export default function RelateSearchItem({ tag, shop_name, menu_name, to }: RelatedSearchItemProps) {
+  const logger = useLogger();
+  const [searchParams] = useSearchParams();
+  const categoryParam = searchParams.get('category');
+
+  const handleSearchItemClick = () => {
+    logger.actionEventClick({
+      team: 'BUSINESS',
+      event_label: 'shop_clicked',
+      value: `${shop_name}`,
+      duration_time: (new Date().getTime() - Number(sessionStorage.getItem('selectedCategoryTime'))) / 1000,
+      previous_page: getCategoryNameById(categoryParam ? Number(categoryParam) : undefined),
+      current_page: `${shop_name}`,
+    });
+  };
+
   return (
-    <Link to={to} className="flex h-12 w-full items-center justify-between border-b border-neutral-200 p-2">
+    <Link
+      to={to}
+      className="flex h-12 w-full items-center justify-between border-b border-neutral-200 p-2"
+      onClick={handleSearchItemClick}
+    >
       <div className="flex items-center gap-1">
         {tag === 'store' ? <StoreIcon /> : <MenuIcon />}
-        <p className="text-[10px] text-neutral-500">{content}</p>
+        <p className="text-[10px] text-neutral-500">{menu_name ? `${menu_name} | ${shop_name}` : shop_name}</p>
       </div>
       <NavigateStoreIcon />
     </Link>
