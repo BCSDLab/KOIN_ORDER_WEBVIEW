535473160015d972fc0bac29240fb0819980146f
jaeik <lji0823@naver.com>
2025-11-13 15:55:46 +0900

    feat: 평균 평점 및 점수 분포 컴포넌트 구현

---
+++ src/pages/Shop/components/AverageRating.tsx
@@ -0,0 +1,33 @@
+import { useParams } from 'react-router-dom';
+import { useShopTotalReview } from '../hooks/useGetShopReview';
+import Rating from './Rating';
+import StarList from './StarList';
+
+export default function AverageRating() {
+  const { shopId } = useParams();
+  const { data: useShopTotalReviews } = useShopTotalReview(shopId!);
+
+  const average = useShopTotalReviews.statistics.average_rating;
+  const ratings = useShopTotalReviews.statistics.ratings;
+  const totalReviewCount = useShopTotalReviews.total_count;
+  const rateList: ['5', '4', '3', '2', '1'] = ['5', '4', '3', '2', '1'];
+
+  return (
+    <div className="flex w-full gap-[29px] border-b border-neutral-300 px-6 pb-[14px]">
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-[32px] leading-[160%]">{average.toFixed(1)}</div>
+        <div className="mt-1">
+          <StarList average_rating={average} />
+        </div>
+      </div>
+
+      <div className="flex flex-1 flex-col">
+        {rateList.map((rate) => {
+          const count = ratings[rate];
+          const fillPercent = totalReviewCount ? (count / totalReviewCount) * 100 : 0;
+          return <Rating key={rate} point={rate} count={count} rate={fillPercent} />;
+        })}
+      </div>
+    </div>
+  );
+}
