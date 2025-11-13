aeb9d4edbf992a340ddf7bb7723da6b30c82ffb1
jaeik <lji0823@naver.com>
2025-11-13 15:57:19 +0900

    feat: Rating 컴포넌트 추가

---
+++ src/pages/Shop/components/Rating.tsx
@@ -0,0 +1,19 @@
+interface RatingProps {
+  point: '5' | '4' | '3' | '2' | '1';
+  rate: number;
+  count: number;
+}
+
+export default function Rating({ point, rate, count }: RatingProps) {
+  return (
+    <div className="inline-flex flex-col items-start">
+      <div className="flex w-full items-center gap-3">
+        <div className="text-[12px] leading-[160%]">{point}점</div>
+        <div className="flex-1 rounded-[10px] bg-neutral-300">
+          <div className="relative h-[6px] rounded-[10px] bg-[#FFC62B]" style={{ width: `${rate}%` }} />
+        </div>
+        <div className="w-[15px] text-[13px] leading-[160%] text-neutral-500">{count}</div>
+      </div>
+    </div>
+  );
+}
