6ef3b691c35d97d327b022216b07976984131724
jaeik <lji0823@naver.com>
2025-11-14 20:51:06 +0900

    feat: rateing 점수 너비와 우측 정렬코드 추가

--- src/pages/Shop/components/Rating.tsx
+++ src/pages/Shop/components/Rating.tsx
@@ -8,7 +8,7 @@ export default function Rating({ point, rate, count }: RatingProps) {
   return (
     <div className="inline-flex flex-col items-start">
       <div className="flex w-full items-center gap-3">
-        <div className="text-[12px] leading-[160%]">{point}점</div>
+        <div className="w-[18px] text-right text-[12px] leading-[160%]">{point}점</div>
         <div className="flex-1 rounded-[10px] bg-neutral-300">
           <div className="relative h-[6px] rounded-[10px] bg-[#FFC62B]" style={{ width: `${rate}%` }} />
         </div>
