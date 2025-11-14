fb409b8ab2520e3fd4de46023b444b27d803adde
jaeik <lji0823@naver.com>
2025-11-14 21:00:18 +0900

    feat: 날짜 포맷 유틸 생성

---
+++ src/util/ts/formatDate.ts
@@ -0,0 +1,8 @@
+export function formatDate(iso: string) {
+  const d = new Date(iso);
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, '0');
+  const day = String(d.getDate()).padStart(2, '0');
+
+  return `${year}.${month}.${day}`;
+}
